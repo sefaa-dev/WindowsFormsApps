28b68ebd46edda0edcd8bfe3de74068b3c94c75a
sefaa-dev <sefayilmaz582@gmail.com>
2021-10-30 21:58:18 +0300

    string birleştirmee

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string ogrno = "58";
-            string ad = "sefa";
-            string soyad = "yılmaz";
-            Console.Write("Öğrenci No:{0} ADI:{1} SOYAD:{2}",ogrno, ad, soyad);
+            //string ogrno = "58";
+            //string ad = "sefa";
+            //string soyad = "yılmaz";
+            //Console.Write("Öğrenci No:{0} ADI:{1} SOYAD:{2}",ogrno, ad, soyad);
+            //Console.ReadKey();
+
+            string tcno = "12345678912";
+            string adsoyad = "Sefa Yılmaz";
+            string gorevi = "Satın Alma Müdürü";
+            double maas = 2560.86;
+            Console.WriteLine("TC KİMLİK NUMARASI:" + tcno + "-" + "Adı Soyadı:" + adsoyad);
+            Console.Write("Görevi:{0} - Maaşı:{1}", gorevi, maas);
             Console.ReadKey();
 
 
