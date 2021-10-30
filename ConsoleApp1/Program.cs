7f3c061e06db528ac2dfc0c5ebece247429728db
sefaa-dev <sefayilmaz582@gmail.com>
2021-10-30 20:37:34 +0300

    string birleştirme

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ogrno = "58";
+            string ad = "sefa";
+            string soyad = "yılmaz";
+            Console.Write("Öğrenci No:{0} ADI:{1} SOYAD:{2}",ogrno, ad, soyad);
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
