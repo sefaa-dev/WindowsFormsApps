c12d163bcec3c3206710469f32a9c8c0af2fbd7c
sefaa-dev <sefayilmaz582@gmail.com>
2021-10-31 09:04:08 +0300

    klavyeden giriş

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,19 +10,39 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            ///STRİNG BİRLEŞTİRME///
+            
             //string ogrno = "58";
             //string ad = "sefa";
             //string soyad = "yılmaz";
             //Console.Write("Öğrenci No:{0} ADI:{1} SOYAD:{2}",ogrno, ad, soyad);
             //Console.ReadKey();
+            //*********************************************************************************************
+            //string tcno = "1234578412";
+            //string adsoyad = "Sefa Yılmaz";
+            //string gorevi = "Satın Alma Müdürü";
+            //double maas = 2560.86;
+            //Console.WriteLine("TC KİMLİK NUMARASI:" + tcno + "-" + "Adı Soyadı:" + adsoyad);
+            //Console.Write("Görevi:{0} - Maaşı:{1}", gorevi, maas);
+            //Console.ReadKey();
+            //*********************************************************************************************
 
-            string tcno = "1234578412";
-            string adsoyad = "Sefa Yılmaz";
-            string gorevi = "Satın Alma Müdürü";
-            double maas = 2560.86;
-            Console.WriteLine("TC KİMLİK NUMARASI:" + tcno + "-" + "Adı Soyadı:" + adsoyad);
-            Console.Write("Görevi:{0} - Maaşı:{1}", gorevi, maas);
-            Console.ReadKey();
+            ///KLAVYEDEN GİRİŞ///
+
+            //int sayi1, sayi2, sonuc;
+            //Console.WriteLine ("1. sayıyı giriniz: ");
+            //sayi1 = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine ("2. sayıyı giriniz: ");
+            //sayi2 = Convert.ToInt16(Console.ReadLine());
+
+            //sonuc = sayi1 + sayi2;
+
+            //Console.Write("Sayıların toplamı; {0}", sonuc);
+            //Console.ReadKey();
+            //*********************************************************************************************
+
+
+
 
 
 
